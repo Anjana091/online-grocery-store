f469a979c92880247e7dd41e88fa13af899ffecb
Anjana091 <anjanasuresh091@gmail.com>
2022-08-06 15:20:54 +0530

    make it better

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,18 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "bgs");
+
+
+//$con=mysqli_connect("localhost", "root", "", "bgs");
+
+//remote database connection 
+
+$host='remotemysql.com';
+$db='C5JW6fOuq6';
+$user='C5JW6fOuq6';
+$pass='gUwo8hxtbh';
+
+
+
+$con=mysqli_connect("$host", "$user", "$pass", "$db");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
